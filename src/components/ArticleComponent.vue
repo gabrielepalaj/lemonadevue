e51e3d5bea835666e48f1f8a480af535e94360ae
gabriele <gabrielepalaj@hotmail.com>
2024-05-04 14:36:34 +0200

    Creato ArticleComponent

---
+++ src/components/ArticleComponent.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="article">
+      <img :src="article.imageUrl" alt="Immagine Articolo">
+      <div class="title">{{ article.title }}</div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'ArticleComponent',
+    props: {
+      article: {
+        type: Object,
+        required: true
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  .article {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-bottom: 20px;
+  }
+  
+  img {
+    width: 100%;
+    max-width: 200px;
+    height: auto;
+  }
+  
+  .title {
+    margin-top: 10px;
+    font-weight: bold;
+  }
+  </style>
+  
